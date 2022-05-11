89c62d10da579284095d4c5e61432f011cd3e63f
aef- <aef@fastmail.com>
2022-05-11 10:56:26 -0400

    Adds support for JACK notifications

--- native/ex_jack/src/lib.rs
+++ native/ex_jack/src/lib.rs
@@ -1,18 +1,19 @@
 //! Elixir NIF to JACK audio server API.
 
 mod atoms;
-use rustler::{Atom, Encoder, Env, Error, NifMap, OwnedEnv, ResourceArc, Term};
+use rustler::{Atom, Encoder, Env, Error, LocalPid, NifMap, OwnedEnv, ResourceArc, Term};
 use std::sync::{mpsc, Mutex};
 use std::{thread, time};
 
 type Sample = f32;
 
-pub struct SenderChannel(Mutex<mpsc::Sender<Vec<Sample>>>);
+pub struct SendFramesChannel(Mutex<mpsc::Sender<Vec<Sample>>>);
 pub struct ShutdownChannel(Mutex<Option<mpsc::Sender<()>>>);
+
 type StartResult = Result<
     (
         Atom,
-        ResourceArc<SenderChannel>,
+        ResourceArc<SendFramesChannel>,
         ResourceArc<ShutdownChannel>,
         Pcm,
     ),
@@ -33,7 +34,7 @@ pub struct Config {
 }
 
 pub fn load(env: Env, _: Term) -> bool {
-    rustler::resource!(SenderChannel, env);
+    rustler::resource!(SendFramesChannel, env);
     rustler::resource!(ShutdownChannel, env);
     true
 }
@@ -47,7 +48,9 @@ pub fn _start(env: Env, config: Config) -> StartResult {
         .register_port("out", jack::AudioOut::default())
         .unwrap();
 
-    let in_port = client.register_port("in", jack::AudioIn::default()).unwrap();
+    let in_port = client
+        .register_port("in", jack::AudioIn::default())
+        .unwrap();
 
     let pid = env.pid();
 
@@ -69,9 +72,7 @@ pub fn _start(env: Env, config: Config) -> StartResult {
 
             if in_frames.len() > 0 {
                 let mut env = OwnedEnv::new();
-                env.send_and_clear(&pid, move |env| {
-                    (atoms::in_frames(), in_frames).encode(env)
-                });
+                env.send_and_clear(&pid, move |env| (atoms::in_frames(), in_frames).encode(env));
             }
 
             let out_frames = out_port.as_mut_slice(ps);
@@ -88,7 +89,9 @@ pub fn _start(env: Env, config: Config) -> StartResult {
     let sample_rate = client.sample_rate();
 
     std::thread::spawn(move || {
-        let active_client = client.activate_async((), process).unwrap();
+        let active_client = client
+            .activate_async(Notifications::create(pid), process)
+            .unwrap();
 
         if config.auto_connect {
             active_client
@@ -108,7 +111,7 @@ pub fn _start(env: Env, config: Config) -> StartResult {
     });
 
     let shutdown_ref = ResourceArc::new(ShutdownChannel(Mutex::new(Some(shutdown_tx))));
-    let sender_ref = ResourceArc::new(SenderChannel(Mutex::new(frames_tx)));
+    let sender_ref = ResourceArc::new(SendFramesChannel(Mutex::new(frames_tx)));
 
     Ok((
         atoms::ok(),
@@ -121,8 +124,88 @@ pub fn _start(env: Env, config: Config) -> StartResult {
     ))
 }
 
+struct Notifications {
+    env_pid: LocalPid,
+}
+
+impl Notifications {
+    fn create(env_pid: LocalPid) -> Notifications {
+        Notifications { env_pid }
+    }
+}
+
+impl jack::NotificationHandler for Notifications {
+    fn shutdown(&mut self, _status: jack::ClientStatus, _reason: &str) {
+        let mut env = OwnedEnv::new();
+        env.send_and_clear(&self.env_pid, move |env| atoms::shutdown().encode(env));
+    }
+
+    fn sample_rate(&mut self, _: &jack::Client, srate: jack::Frames) -> jack::Control {
+        let mut env = OwnedEnv::new();
+        env.send_and_clear(&self.env_pid, move |env| {
+            (atoms::sample_rate(), srate).encode(env)
+        });
+
+        jack::Control::Continue
+    }
+
+    fn client_registration(&mut self, _: &jack::Client, name: &str, is_reg: bool) {
+        let mut env = OwnedEnv::new();
+        if is_reg {
+            env.send_and_clear(&self.env_pid, move |env| {
+                (atoms::client_register(), name).encode(env)
+            });
+        } else {
+            env.send_and_clear(&self.env_pid, move |env| {
+                (atoms::client_unregister(), name).encode(env)
+            });
+        }
+    }
+
+    fn port_registration(&mut self, client: &jack::Client, port_id: jack::PortId, is_reg: bool) {
+        if let Some(port) = client.port_by_id(port_id) {
+            let mut env = OwnedEnv::new();
+            if is_reg {
+                env.send_and_clear(&self.env_pid, move |env| {
+                    (atoms::port_register(), port_id, port.name().unwrap_or("<unknown>".to_owned())).encode(env)
+                });
+            } else {
+                env.send_and_clear(&self.env_pid, move |env| {
+                    (atoms::port_unregister(), port_id).encode(env)
+                });
+            }
+        }
+    }
+
+    fn ports_connected(
+        &mut self,
+        _: &jack::Client,
+        port_id_a: jack::PortId,
+        port_id_b: jack::PortId,
+        are_connected: bool,
+    ) {
+        let mut env = OwnedEnv::new();
+        if are_connected {
+            env.send_and_clear(&self.env_pid, move |env| {
+                (atoms::ports_connected(), port_id_a, port_id_b).encode(env)
+            });
+        } else {
+            env.send_and_clear(&self.env_pid, move |env| {
+                (atoms::ports_disconnected(), port_id_a, port_id_b).encode(env)
+            });
+        }
+    }
+
+    fn xrun(&mut self, _: &jack::Client) -> jack::Control {
+        let mut env = OwnedEnv::new();
+        env.send_and_clear(&self.env_pid, move |env| atoms::xrun().encode(env));
+
+        jack::Control::Continue
+    }
+}
+
 #[rustler::nif]
-fn send_frames(resource: ResourceArc<SenderChannel>, frames: Vec<Sample>) -> Atom {
+fn send_frames(resource: ResourceArc<SendFramesChannel>, frames: Vec<Sample>) -> Atom {
     let arc = resource.0.lock().unwrap().clone();
     let _ = arc.send(frames);
     atoms::ok()
